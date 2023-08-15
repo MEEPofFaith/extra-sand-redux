f12cfedf8b6c36134f64826e851897ed2b6aaa66
MEEPofFaith <meepmatthew@gmail.com>
2023-08-15 09:04:52 -0700

    Yoink build stuff from PM
    It's not working aaaa
    
    (Switch to Glenn Entity Annos later)

---
+++ src/extrasand/ExtraSand.java
@@ -0,0 +1,15 @@
+package extrasand;
+
+import mindustry.mod.*;
+
+public class ExtraSand extends Mod{
+
+    public ExtraSand(){
+
+    }
+
+    @Override
+    public void loadContent(){
+        
+    }
+}
